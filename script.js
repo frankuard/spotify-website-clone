14058ba1c97490b52a158e4554e37ebcb7611d0d
Roshan Karki <roshankarki4956@gmail.com>
2026-06-15 21:06:08 +0545

    Adding the list of song names

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 console.log("Welcome to Spotify");
 
+
+let songs = [{
+    songName: "Pretty Girls Make Graves", filPath: "audio/1.m4a", coverPath: "covers/The_Smiths_The_Smiths.jpg",
+    songName: "Pretty Girls Make Graves", filPath: "audio/1.m4a", coverPath: "covers/The_Smiths_The_Smiths.jpg",
+    songName: "Pretty Girls Make Graves", filPath: "audio/1.m4a", coverPath: "covers/The_Smiths_The_Smiths.jpg",
+    songName: "Pretty Girls Make Graves", filPath: "audio/1.m4a", coverPath: "covers/The_Smiths_The_Smiths.jpg",
+    songName: "Pretty Girls Make Graves", filPath: "audio/1.m4a", coverPath: "covers/The_Smiths_The_Smiths.jpg",
+    songName: "Pretty Girls Make Graves", filPath: "audio/1.m4a", coverPath: "covers/The_Smiths_The_Smiths.jpg",
+}]
 let audioElement = new Audio('audio/1.m4a');
 //audioElement.play();
